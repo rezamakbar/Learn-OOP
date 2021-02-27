2211048b5b41160d2d47f8fd083a7a31fb963a60
rezamakbar <rezamakbar13@gmail.com>
2021-02-27 15:08:45 +0700

    lewat vscode

---
+++ 49.tanpa_getter_dan_setter.php
@@ -0,0 +1,15 @@
+<?php
+class Produk 
+{ 
+    public $merek;
+    public $stok;
+}
+
+$produk01 = new Produk();
+$produk01->merek = "Acer";
+$produk01->stok = 10;
+
+echo $produk01->merek;
+echo "<br>";
+echo $produk01->stok;
+
