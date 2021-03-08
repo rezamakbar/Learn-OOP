089b984937c2a1d0524c7e9d9f693f87eb1c9ffb
unknown <rezamakbar13@gmail.com>
2021-03-08 17:32:33 +0700

    Reza

---
+++ 51.setter_validation.php
@@ -0,0 +1,57 @@
+<?php
+
+    class Produk {
+        private $stok = 0;
+        
+        public function setStok($stok){    
+            if(is_int($stok)) {
+                $this->stok = $stok;
+            }
+            else {
+                echo "Error, Stok harus angka bulat <br>";
+            }
+        }
+
+        public function getStok(){
+            return $this->stok;
+        }
+
+    }
+
+    $produk01 = new Produk;
+    echo $produk01->getStok();
+    echo "<br>";
+
+    $produk01->setStok("10.5");
+    echo $produk01->getStok();
+    echo '<br>';
+
+    $produk01->setStok("satu");
+    echo $produk01->getStok();
+    echo'<br>';
+
+    $produk01->setStok(10);
+    echo $produk01->getStok();
+    echo '<br>';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
